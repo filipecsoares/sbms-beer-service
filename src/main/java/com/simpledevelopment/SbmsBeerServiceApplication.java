550e12954e8c819ea98b66beb651c7fde3b0a9d4
Filipe Soares <filipecarlossoares@gmail.com>
2020-12-21 10:40:17 -0300

    first commit

---
+++ src/main/java/com/simpledevelopment/SbmsBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.simpledevelopment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbmsBeerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbmsBeerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simpledevelopment/SbmsBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simpledevelopment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbmsBeerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
