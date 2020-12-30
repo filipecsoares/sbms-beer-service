3868cb683ef5f6f7c8338ae789efbe47bc988a69
Filipe Soares <filipecarlossoares@gmail.com>
2020-12-30 09:44:38 -0300

    Adding Jackson Config for BeerDto

--- src/main/java/com/simpledevelopment/web/model/BeerDto.java
+++ src/main/java/com/simpledevelopment/web/model/BeerDto.java
@@ -9,6 +9,9 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Null;
 import javax.validation.constraints.Positive;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonFormat.Shape;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -25,8 +28,10 @@ public class BeerDto {
 	@Null
 	private Integer version;
 	@Null
+	@JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
 	private OffsetDateTime createdDate;
 	@Null
+	@JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
 	private OffsetDateTime lastModifiedDate;
 	@NotBlank
 	private String beerName;
@@ -35,6 +40,7 @@ public class BeerDto {
 	@Positive
 	@NotNull
 	private Long upc;
+	@JsonFormat(shape = Shape.STRING)
 	@Positive
 	@NotNull
 	private BigDecimal price;
