8a13d1387284fde1a0450d82ef4e3e9b61018f6e
Filipe Soares <filipecarlossoares@gmail.com>
2021-01-01 13:16:02 -0300

    initializing data via data.sql file

--- src/main/java/com/simpledevelopment/bootstrap/BeerLoader.java
+++ src/main/java/com/simpledevelopment/bootstrap/BeerLoader.java
@@ -1,19 +1,20 @@
 package com.simpledevelopment.bootstrap;
 
-import java.math.BigDecimal;
+import java.util.UUID;
 
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
 
-import com.simpledevelopment.domain.Beer;
 import com.simpledevelopment.repository.BeerRepository;
 
-@Component
+//@Component
 public class BeerLoader implements CommandLineRunner {
 
 	public static final String BEER_1_UPC = "0631234200036";
 	public static final String BEER_2_UPC = "0631234300019";
 	public static final String BEER_3_UPC = "0083783375213";
+	public static final UUID BEER_1_UUID = UUID.fromString("0a818933-087d-47f2-ad83-2f986ed087eb");
+	public static final UUID BEER_2_UUID = UUID.fromString("a712d914-61ea-4623-8bd0-32c0f6545bfd");
+	public static final UUID BEER_3_UUID = UUID.fromString("026cc3c8-3a0c-4083-a05b-e908048c1b08");
 
 	private final BeerRepository beerRepository;
 
@@ -24,19 +25,7 @@ public class BeerLoader implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		loadBeerObjects();
-	}
-
-	private void loadBeerObjects() {
-		if (beerRepository.count() == 0) {
-			beerRepository.save(Beer.builder().beerName("Mango Bob").beerStyle("IPA").quantityToBrew(200).minOnHand(12)
-					.upc(BEER_1_UPC).price(new BigDecimal(12.95)).build());
-
-			beerRepository.save(Beer.builder().beerName("Galaxi Cat").beerStyle("PALE_ALE").quantityToBrew(200)
-					.minOnHand(12).upc(BEER_2_UPC).price(new BigDecimal(11.95)).build());
-
-			beerRepository.save(Beer.builder().beerName("No Hammers On The Bar").beerStyle("PALE_ALE")
-					.quantityToBrew(200).minOnHand(12).upc(BEER_3_UPC).price(new BigDecimal(11.95)).build());
-		}
+		// using data.sql file now
+		// loadBeerObjects();
 	}
 }
